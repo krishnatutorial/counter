d5c7b6d16cb17d8021b7153fa1746e72a9625da3
krishnatutorial <krishnakjp963@gmail.com>
2024-03-14 13:42:17 +0000

    Code giving error

---
+++ src/components/Counter/index.js
@@ -0,0 +1,17 @@
+import {Component} from 'react'
+import './index.CSS'
+class Counter extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="heading">Counter</h1>
+        <p className="count">0</p>
+        <div>
+          <button className="button">Increase</button>
+          <button className="button">Decrease</button>
+        </div>
+      </div>
+    )
+  }
+}
+export default Counter
